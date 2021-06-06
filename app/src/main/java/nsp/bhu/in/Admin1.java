9460d42df629f8cf547bb29bd5685e780660fbc3
Shreygupta2002 <shrey.gupta.mat20@itbhu.ac.in>
2021-06-06 12:46:10 +0530

    first commit

---
+++ app/src/main/java/nsp/bhu/in/Admin1.java
@@ -0,0 +1,14 @@
+package nsp.bhu.in;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Admin1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin2);
+    }
+}
